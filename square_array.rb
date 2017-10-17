1b492ab6f08070d567fe473ac3ce53f365fd5186
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 17:59:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
+#def square_array(array)
+#  squared = []
+#  array.each { |num| squared << num*num }
+#  squared
+#end
+
 def square_array(array)
-  squared = []
-  array.each { |num| squared << num*num  }
-  squared
+  array.collect { |num| num*num }
 end
